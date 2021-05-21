cc0dd53ff0770b6f095c029c97fbb0295287a9eb
NelsonNeto16 <nelson171782@gmail.com>
2021-05-20 20:21:14 -0400

    books

---
+++ app/Http/Controllers/Api/BookController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    private $book;
+
+    public function __construct(Book $book)
+    {
+        $this->book = $book;
+    }
+
+    public function index()
+    {
+        try
+        {
+            $books = $this->book->all();
+            return response()->json(['data' => $books], 200);
+        }
+        catch(\Exception $e)
+        {
+            return response()->json(['data' => ['msg' => $e->getMessage()]]);
+        }      
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        try
+        {
+            $book = $this->book->create($data);
+            return response()->json(['data' => ['msg' => 'Livro cadastrado com sucesso!']], 200);
+        }
+        catch(\Exception $e)
+        {
+            return response()->json(['data' => ['msg' => $e->getMessage()]]);
+        }
+    }
+
+    public function update($id, Request $request)
+    {
+        $data = $request->all();
+        try
+        {
+            $book = $this->book->find($id);
+            if(!$book)
+            {
+                return response()->json(['data' => ['msg' => 'Livro não encontrado']]);
+            }
+            $book->update($data);
+            return response()->json(['data' => ['msg' => 'Livro atualizado com sucesso!']], 200);
+        }
+        catch(\Exception $e)
+        {
+            return response()->json(['data' => ['msg' => $e->getMessage()]]);
+        }
+    }
+
+    public function destroy($id)
+    {
+        try
+        {
+            $book = $this->book->find($id);
+            if(!$book)
+            {
+                return response()->json(['data' => ['msg' => 'Livro não encontrado']]);
+            }
+            $book->delete();
+            return response()->json(['data' => ['msg' => 'Livro apagado com sucesso!']], 200);
+        }
+        catch(\Exception $e)
+        {
+            return response()->json(['data' => ['msg' => $e->getMessage()]]);
+        }
+    }
+
+    public function show($id)
+    {
+        try
+        {
+            $book = $this->book->find($id);
+            if(!$book)
+            {
+                return response()->json(['data' => ['msg' => 'Livro não encontrado']]);
+            }
+            $book->user;
+            return response()->json(['data' => $book], 200);
+        }
+        catch(\Exception $e)
+        {
+            return response()->json(['data' => ['msg' => $e->getMessage()]]);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\BookController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::resource('users', UserController::class);
+Route::resource('books', BookController::class);
